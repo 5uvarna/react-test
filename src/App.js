7f514fa30dc714ff6de0fbeef76e944bff9f24b9
Ashish <ashish@Ashishs-MacBook-Pro.local>
2023-03-21 14:44:18 +0530

    pukli2

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React and Firebase @MN
+          Learn React and Firebase
         </a>
       </header>
     </div>
